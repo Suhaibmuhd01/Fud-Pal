c092e9acf702563efb7f3646ff94f5ae1f35d513
Suhaibmuhd01 <148004278+Suhaibmuhd01@users.noreply.github.com>
2025-08-22 20:57:43 +0100

    removed previous code to debug

--- Database/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn =mysqli_connect("localhost",'root',"","Fud-pal") or die(mysqli_connect_error());
-?>
--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-function back() {
-    const btnAction = document.getElementById("btn");
-    btnAction.onclick = window.self.history.back();
-}
-back();
